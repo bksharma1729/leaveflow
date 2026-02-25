9434cdebb7312c4531298b0472c1fa8fedc7833f
Bablu Kumar Sharma <babluks2244@gmail.com>
2026-02-25 23:56:22 +0530

    feat: implement user registration with JWT

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-
+const authRoutes = require("./src/routes/authRoutes");
 dotenv.config();
 
 // Connect to database
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 // Test Route
 app.get("/", (req, res) => {
   res.send("🚀 LeaveFlow API is running...");
